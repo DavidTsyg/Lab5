be000c63c85d64af69792f01badb1d3bb52a514c
DavidTsyg <davidtsyg@gmail.com>
2017-04-20 22:25:08 +0300

    Update Sem2_Lab5.hpp

--- include/Sem2_Lab5.hpp
+++ include/Sem2_Lab5.hpp
@@ -1,71 +1,202 @@
+#include "stdafx.h"
 #include <iostream>
-#include <stdlib.h>
 using namespace std;
- 
-template <typename T>
+template <class T>
 class binarytree
 {
+protected:
+	//структура узла	
+	struct node
+	{
+		T key_value;
+		//		unsigned int height;
+		node* left;
+		node* right;
+		node(T k) { key_value = k; left = right = 0; /*height = 1;*/ }
+	};
 
-public: 
-	T key;
-int insert(T key, binarytree *root)		
- {		
- 	if (!root)		
- 	{		
- 		root = new binarytree;		
- 		root->key_value = key;		
- 		return root->key_value;		
- 	}		
- 	else if (root->key_value > key) insert(key, root);		
- 	else if (root->key_value < key) insert(key, root); 		
- 	else return root->key_value;		
- }		
+	//инициализируем корень дерева	
+	node* root = NULL;
+	//вспомогательная функция вставки
+	node* insert_serve(node*& leaf, T key)
+	{
+		if (leaf == NULL)
+			leaf = new node(key);
+		else if (key < leaf->key_value)
+			insert_serve(leaf->left, key);
+		else if (key > leaf->key_value)
+			insert_serve(leaf->right, key);
+		return leaf;
+	}
+	//вспомогательная функция поиска ключа
+	virtual node* search_serve(T key, node* leaf)
+	{
+		if (leaf != NULL)
+		{
+			if (key == leaf->key_value) return leaf;
+			else if (key < leaf->key_value)//если заданный ключ меньше ключа в узле то идем влево
+				return search_serve(key, leaf->left);//рекурсивно идем к следующему узлу
+			else if (key > leaf->key_value)//иначе идем вправо
+				return search_serve(key, leaf->right);//рекурсивно идем к следующему узлу
+		}
+		else return NULL;//если ничего не нашли то возвращеам NULL
+	}
+	//вспомогательная функция уничтожения дерева
+	void destroytree_serve(node* leaf)
+	{
+		if (leaf != NULL)//проверка
+		{
+			destroytree_serve(leaf->left);//рекурсивно идем к следующему узлу
+			destroytree_serve(leaf->right);//рекурсивно идем к следующему узлу
+			delete leaf;//уничтожаем узел
+		}
+	}
+	//вспомогательная функция нахождения пути к узлу
+	void printpath_serve(T key, node *leaf)
+	{
+		if (key == leaf->key_value) cout << leaf->key_value << endl;
+		else if (key < leaf->key_value)//если заданный ключ меньше ключа в узле то идем влево
+		{
+			cout << leaf->key_value << endl;
+			printpath_serve(key, leaf->left);//рекурсивно идем к следующему узлу
+		}
+		else if (key > leaf->key_value)//иначе идем вправо
+		{
+			cout << leaf->key_value << endl;
+			printpath_serve(key, leaf->right);//рекурсивно идем к следующему узлу
+		}
+	}
 
-int print(int check1, binarytree *root)		
- {		
- 	if (!root) return check1; 		
- 	if (root->key_value == 10 ) check1 = 12;		
- 			
- 	return check1;		
- }
+	//вспомогательная функция находящая минимальный ключ в "под-ветке" заданного ключа 
+	node* findmin(node* leaf)
+	{
+		if (leaf->left != NULL)//проверка на существование меньшего значения
+			findmin(leaf->left);//рекурсивно идем к следующему узлу
+		else return leaf;
+	}
+	//вспомогательная функция уничтожающая узел 
+	node* destroynode_serve(node* leaf, T key)
+	{
+		if (leaf == NULL)
+			return leaf;
+		if (key < leaf->key_value)
+			leaf->left = destroynode_serve(leaf->left, key);
+		else if (key > leaf->key_value)
+			leaf->right = destroynode_serve(leaf->right, key);
+		else
+		{
+			if ((leaf->left == NULL) || (leaf->right == NULL))
+			{
+				node* temp = leaf->left ? leaf->left :
+					leaf->right;
 
-int search(int check2, binarytree *root)		
- {		
- 	if (!root) return check2;		
- 		
- 	if(root->key_value >= 0) 		
- 		{ 		
- 			root->key_value = 1;		
- 		}		
- 			
- 	return root->key_value;		
- 		
- }
+				// нет детей
+				if (temp == NULL)
+				{
+					temp = leaf;
+					leaf = NULL;
+				}
+				else // один ребенок
+					*leaf = *temp;
+				delete temp;
+			}
+			else
+			{
+				//два ребенка
+				node* temp = findmin(leaf->right);
+				leaf->key_value = temp->key_value;
+				leaf->right = destroynode_serve(leaf->right, temp->key_value);
+			}
+		}
+		if (leaf == NULL)
+			return leaf;
+		return leaf;
+	}
+	//вспомогательная функция вывода дерева
+	void print_tree(node* leaf, int level)
+	{
+		if (leaf != NULL) {
+			print_tree(leaf->left, level + 1);     // вывод левого "под-дерева"
+			for (int i = 0; i < level; i++) cout << "   ";
+			cout << leaf->key_value << endl;               // вывод корня поддерева
+			print_tree(leaf->right, level + 1);    // вывод правого "под-дерева"
+		}
+	}
 
-int destroy_node(int check3, binarytree *root)		
- {		
- 	if (!root) return check3;		
- 			
- 	cout << endl << endl << "Type the node you want to delete: "; 		
- 		
- 	if (root->key_value == check3) 		
- 		{ 		
- 			cout << endl << "[-] Error: You are trying to remove the root of the tree!" << endl;		
- 			return 0;		
- 		}		
- 		
- 			
- }		
- 
-int destroy_tree(int check4, binarytree *root)		
- {		
- 	if (root != NULL)		
- 	{		
- 		delete root;		
-		root = NULL;		
-	}		
-			
-	return check4;		
-}
+public:
+	binarytree() {}
+	~binarytree() { destroytree(); }
+	//основная функция для вставки ключа	
+	int insert(T key)
+	{
+		if (search_serve(key, root) == NULL)
+		{
+			if (root == NULL) {
+				root = new node(key);
+			}
+			else
+				insert_serve(root, key);
+			return 0;
+		}
+		else
+		{
+			cout << "Key " << key << " already exists !" << endl;
+			return 1;
+		}
+	}
+	//основная функция для нахождения ключа
+	int search(T key)
+	{
+		if (search_serve(key, root) != NULL)
+		{
+			cout << "Key " << key << " exists" << endl;//используем вспомогательную функцию
+			return 0;
+		}
+		else
+		{
+			cout << "Key doesn't exist" << endl;
+			return 1;
+		}
+	}
+	//основная функция для удаления дерева
+	void destroytree()
+	{
+		destroytree_serve(root);//используем вспомогательную функцию
+	}
+	//основная функция для удаления узла
+	int destroynode(T key)
+	{
+		
+		//находим узел для вспомогательной функции
+		if (search_serve(key, root) != NULL)
+		{
+			destroynode_serve(root, key);//используем вспомогательную функцию
+			return 0;
+		}
+		else
+		{
+			cout << "Key " << key << " doesn't exist !" << endl;
+			return 1;
+		}
+	}
+	//основная функция вывода пути к узлу
+	void printpath(T key)
+	{
+		if (search_serve(key, root) != NULL)//проверка
+		{
+			cout << endl << endl << endl;
+			printpath_serve(key, root);//используем вспомогательную функцию
+			cout << endl << endl << endl;
+		}
+		else
+			cout << "Key " << key << " doesn't exist" << endl;
+	}
+	//основная функция вывода дерева
+	void print()
+	{
+		cout << endl << endl << endl << endl;
+		print_tree(root, 0);
+		cout << endl << endl << endl << endl;
+	}
 
 };
